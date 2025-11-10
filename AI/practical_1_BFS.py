7ac1ac68a11ae7340b2749d7069a096407286b7f
Swayam Prashant Jagtap <professional00one@gmail.com>
2025-11-11 00:42:38 +0530

    AI practical 1 and 2 completed

--- AI/practical_1_BFS.py
+++ AI/practical_1_BFS.py
@@ -1,5 +1,6 @@
 from collections import deque
 
+#with set()----------
 graph1 = {
     'A': set(['B', 'C']),
     'B': set(['A', 'D', 'E']),
@@ -19,3 +20,26 @@ def bfs(graph, start):
             queue.extend(graph[node]-set(visited))
     return visited
 print(bfs(graph1, 'A'))
+
+#without set()----------
+graph2 = {
+    'A': ['B', 'C'],
+    'B': ['A', 'D', 'E'],
+    'C': ['A', 'F'],
+    'D': ['B'],
+    'E': ['B', 'F'],
+    'F': ['C', 'E']
+}
+def bfs_no_set(graph, start):
+    visited = []
+    queue = deque([start])
+    while queue:
+        node = queue.popleft()
+        if node not in visited:
+            visited.append(node)
+            for neighbor in sorted(graph[node]):
+                if neighbor not in visited and neighbor not in queue:
+                    queue.append(neighbor)
+    return visited
+print(bfs_no_set(graph2, 'A')) 
+# Output: ['A', 'B', 'C', 'D', 'E', 'F']
--- AI/practical_2_DFS.py
+++ AI/practical_2_DFS.py
@@ -9,4 +9,29 @@ graph1 = {
     'E': set(['B', 'F']),
     'F': set(['C', 'E'])
 }
+def dfs(graph, node, visited):
+    if node not in visited:
+        visited.append(node)
+        for neighbor in graph[node]:
+            dfs(graph, neighbor, visited)
+    return visited
+visited = dfs(graph1, 'A', [])
+print(visited) 
 
+#without sets--------------------
+graph2 = {
+    'A': ['B', 'C'],
+    'B': ['A', 'D', 'E'],
+    'C': ['A', 'F'],
+    'D': ['B'],
+    'E': ['B', 'F'],
+    'F': ['C', 'E']
+}
+def dfs_no_set(graph, node, visited):
+    if node not in visited:
+        visited.append(node)
+        for neighbor in graph[node]:
+            dfs_no_set(graph, neighbor, visited)
+    return visited
+visited_no_set = dfs_no_set(graph2, 'A', [])
+print(visited_no_set)  # Output: ['A', 'B', 'D', 'E', 'F', 'C']
