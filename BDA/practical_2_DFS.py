024eeb9d51172099d2a540b502ca2244d4d76083
Swayam Prashant Jagtap <professional00one@gmail.com>
2025-11-10 12:36:01 +0000

    first commit message

---
+++ BDA/practical_2_DFS.py
@@ -0,0 +1,12 @@
+from collections import deque
+
+#with sets--------------------
+graph1 = {
+    'A': set(['B', 'C']),
+    'B': set(['A', 'D', 'E']),
+    'C': set(['A', 'F']),
+    'D': set(['B']),
+    'E': set(['B', 'F']),
+    'F': set(['C', 'E'])
+}
+
